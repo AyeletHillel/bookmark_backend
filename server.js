65445d7266cad0177b8ef894dd4fe8003c2d8b05
Ayelet Hillel <ah3708@tc.columbia.edu>
2023-01-17 21:05:23 -0500

    backend completed

---
+++ server.js
@@ -0,0 +1,113 @@
+///////////////////////////////
+// DEPENDENCIES
+////////////////////////////////
+// get .env variables
+require("dotenv").config();
+// pull PORT from .env, give default value of 3000
+const { PORT = 3000, DATABASE_URL } = process.env;
+// import express
+const express = require("express");
+// create application object
+const app = express();
+// import mongoose
+const mongoose = require("mongoose");
+// import middlware
+const cors = require("cors");
+const morgan = require("morgan");
+
+///////////////////////////////
+// DATABASE CONNECTION
+////////////////////////////////
+// Establish Connection
+mongoose.connect(DATABASE_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
+  // Connection Events
+  mongoose.connection
+    .on("open", () => console.log("Your are connected to mongoose"))
+    .on("close", () => console.log("Your are disconnected from mongoose"))
+    .on("error", (error) => console.log(error));
+
+///////////////////////////////
+// MODELS
+////////////////////////////////
+const bookMarkSchema = new mongoose.Schema({
+  title: String,
+  url: String,
+});
+
+const bookMark = mongoose.model("bookMark", bookMarkSchema);
+
+///////////////////////////////
+// MiddleWare
+////////////////////////////////
+app.use(cors()); // to prevent cors errors, open access to all origins
+app.use(morgan("dev")); // logging
+app.use(express.json()); // parse json bodies
+
+///////////////////////////////
+// ROUTES
+////////////////////////////////
+// create a test route
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+//index route
+app.get("/bookmark", async (req, res) => {
+  try {
+    // send all bookmarks
+    res.json(await bookMark.find({}));
+  } catch (error) {
+    //send error
+    res.status(400).json(error);
+  }
+});
+
+//create route
+app.post("/bookmark", async (req, res) => {
+  try {
+    res.json(await bookMark.create(req.body));
+  } catch (error) {
+    //send error
+    res.status(400).json(error);
+  }
+});
+
+//updtae route
+app.put("/bookmark/:id", async (req, res) => {
+  try {
+    res.json(
+      await bookMark.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    );
+  } catch (error) {
+    //send error
+    res.status(400).json(error);
+  }
+});
+
+//delete route
+app.delete("/bookmark/:id", async (req, res) => {
+  try {
+    res.json(await bookMark.findByIdAndRemove(req.params.id));
+  } catch (error) {
+    //send error
+    res.status(400).json(error);
+  }
+});
+
+//show route
+app.get("/bookmark/:id", async (req, res) => {
+  try {
+    res.json(await bookMark.findById(req.params.id));
+  } catch (error) {
+    //send error
+    res.status(400).json(error);
+  }
+});
+
+///////////////////////////////
+// LISTENER
+////////////////////////////////
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
